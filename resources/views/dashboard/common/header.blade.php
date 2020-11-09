bb00a513564518b54cb2d1864a665bf39c94704c
mcespede <cespedes.mauro@gmail.com>
2020-11-09 13:21:53 +0100

    Inclui en el header la libreria de JQUERY

--- resources/views/dashboard/common/header.blade.php
+++ resources/views/dashboard/common/header.blade.php
@@ -17,11 +17,12 @@
 
 <!-----------------------DATATABLES-ADITIONALS------------------------------------>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.21/b-1.6.2/b-colvis-1.6.2/b-flash-1.6.2/b-html5-1.6.2/b-print-1.6.2/cr-1.5.2/fh-3.1.7/kt-2.5.2/r-2.2.5/sp-1.1.1/sl-1.3.1/datatables.min.css"/>
- 
+
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.21/b-1.6.2/b-colvis-1.6.2/b-flash-1.6.2/b-html5-1.6.2/b-print-1.6.2/cr-1.5.2/fh-3.1.7/kt-2.5.2/r-2.2.5/sp-1.1.1/sl-1.3.1/datatables.min.js"></script>
 
-
+<!-- Esta es la libreria de jQUERY para que funcione las llamadas del MQTT-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
