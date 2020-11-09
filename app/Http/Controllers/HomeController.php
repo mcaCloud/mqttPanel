cf6023b08d8fb077adf9c3ca044d70f7c9c8bfc7
mcespede <cespedes.mauro@gmail.com>
2020-11-09 10:55:20 +0100

    Modifiqué el HomeController para que no llame a los archivos que aún no conecto a la base de datos. Sino me sigue dando error y la info queda expuesta.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,29 +40,29 @@ class HomeController extends Controller
         /*$docs = DB::table('docs')->paginate(5);
         return view('welcome');*/
         /*Tambien lo puedo hacer utilizando el modelo, de esta forma debo de importar el modelo FILE en el controlador. Tambien utilizo el ORDER BY para ordenar los videos de mas nuevo a mas antiguo*/
-        $files = File::orderBy('id','desc')-> paginate(5);
+        //$files = File::orderBy('id','desc')-> paginate(5);
         /*Ahora le tengo que pasar la informacion a la vista, para eso le paso un array al VIEW*/
 
-        //Como en la vista de Index tengo tabs y cada una tiene informacion diferente los que voy a hacer es hacer un query en cada tabla y pasarle la info de todas las tablas que ocupo a la vista. 
-        $videos = Video::orderBy('id','desc')-> paginate(5);
+        //Como en la vista de Index tengo tabs y cada una tiene informacion diferente los que voy a hacer es hacer un query en cada tabla y pasarle la info de todas las tablas que ocupo a la vista.
+        //$videos = Video::orderBy('id','desc')-> paginate(5);
 
-        $docs = Doc::orderBy('id','desc')-> paginate(5);
-        
-        $users = User::orderBy('id','desc')-> paginate(5);
+        //$docs = Doc::orderBy('id','desc')-> paginate(5);
 
-        $alerts = Alert::orderBy('id','desc')-> paginate(5);
-        
-        $employees = EmployeeInfo::orderBy('id','desc')-> paginate(5);
+        //$users = User::orderBy('id','desc')-> paginate(5);
+
+        //$alerts = Alert::orderBy('id','desc')-> paginate(5);
+
+        //$employees = EmployeeInfo::orderBy('id','desc')-> paginate(5);
         //Ahora lo que tengo que hacer es pasarle a la vista toda la informacion de estos objetos de la base de datos.
-        //Le paso los tre por si tengo que intercalar en 
+        //Le paso los tre por si tengo que intercalar en
         return view ('welcome',array(
             /*Creo un indice FILES y le paso todos los files, de esta forma ya tengo accesible todos los files en la vista welcome*/
-            'files' =>$files,
-            'videos' =>$videos,
-            'docs' =>$docs,
+            //'files' =>$files,
+            //'videos' =>$videos,
+            //'docs' =>$docs,
             'users' =>$users,
-            'employees' =>$employees,
-            'alerts' => $alerts
+            //'employees' =>$employees,
+            //'alerts' => $alerts
         ));
 
 
