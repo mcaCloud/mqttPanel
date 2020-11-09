1db2bbea711860821f1c460e5217d5be189a4453
mcespede <cespedes.mauro@gmail.com>
2020-11-09 11:49:00 +0100

    Como le hice un Fetch cunado installé el ATOM se me borraron los setting del mqtt del servidor. Tengo que volver a incluir el script de JS para continuar

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -84,4 +84,56 @@
 <!------------------------PAGE------------------------------>
 <!----------------------------------------------------------->
 
+<!----------------------------------------------------------->
+<!------------------------JS SCRIPTS MQTT-------------------->
+<!----------------------------------------------------------->
+
+<!--Voy a escribir aqui los scripts del MQTT porque en el layout principla
+están tambien los del navigation bar y se me puede confundir. ENtonces mejor
+segmentarlo de esta forma.-->
+
+<!-- Primero escribo la librería-->
+<script src="https://unpkg.com/mqtt/dist/mqtt.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+      // connect options
+	    const options = {
+      		connectTimeout: 3000,
+
+                // Authentication
+      		 clientId: 'emqx',
+      		 username: '',
+      		 password: '',
+
+      		 keepalive: 60,
+      		 clean: true,
+	      }
+
+	    //YA intalle el cetificado y ahora entonces puedo utilizar el protocolo WSS
+	    const WebSocket_URL = 'wss://gaia2-0.com:8094/mqtt'
+
+	    //Aqui llamo a la libreria que llame al principio
+	    //La libreria la estoy llamando desde una pagina, pero la puedo descargar al servidor para evitar problemas por si se cae la pagina
+	    //A la libreria le paso dos parametros
+            //A donde se tiene que conectar : WebSockets_URL. Es la variable que iniciamos arriba
+            // Las opciones que seteamos mas arriba. Es el array con los parametros de coneccion
+
+	    const client = mqtt.connect(WebSocket_URL, options)
+
+           // ///////////EVENTOS ////////////////
+	   // Estos no los defino yo, vienen predefinidos por la libreria
+	   //Basicamente es lo que paso en caso de conectarme, Errores o reconeccion
+
+	    client.on('connect', () => {
+    		console.log('connect success exito')
+	     })
+
+	    client.on('reconnect', (error) => {
+    		console.log('reconnecting:', error)
+	     })
+
+	    client.on('error', (error) => {
+    	        console.log('Connect Error:', error)
+	    })
+
+	</script>
 @stop
