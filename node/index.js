9ac3890d2a052971e20c66542d8c6ad080b6cc9c
mcespede <cespedes.mauro@gmail.com>
2020-11-11 18:26:04 +0100

    Comienzo a editar el archivo de index.js . Ejecuto una prueba para demostrar que las librerias no esta installadas

--- node/index.js
+++ node/index.js
@@ -1,1 +1,5 @@
-console.log("IoT!");
+//Lo primero que hago es llamar las librerias de MySql y mqtt
+//Creo una variable para que me guarde la peticion de requerir esas librerias
+var mysql = require('mysql');
+//Para MQTT vamos a utilizar la libreria que me recomienda EMQX, aunque tambien podriamos utilizar -Paho mqtt-
+var mqtt = require('mqtt');
